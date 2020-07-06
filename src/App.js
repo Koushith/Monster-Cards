e55d7f530b5234ff8ec232dd8e8a981f54ca2cd2
koushith <koushith97@gmail.com>
2020-07-06 22:39:30 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-import React, { Component } from 'react';
-import { CardList } from './components/card-list/card-list-component';
+import React from 'react';
+
 import './App.css';
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-    };
-  }
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users }))
-      .catch((err) => console.log('Therre was an error', err));
-  }
-  render() {
-    return (
-      <div className='App'>
-        <CardList
-          name={this.state.monsters.map((monster) => (
-            <h1 key={monster.id}>{monster.name}</h1>
-          ))}
-        />
-
-        {/* </CardList> */}
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <h1>Hello</h1>;
+};
 
 export default App;
--- src/components/card-list/card-list-component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './card-list-styles.css';
-export const CardList = ({ name }) => {
-  return (
-    <div className='card-list'>
-      <h2>Hello From {name}</h2>
-    </div>
-  );
-};
