b0c4d77079e2393cffbd371f354759f7f769593b
koushith <koushith97@gmail.com>
2020-09-12 02:04:43 +0530

    adds navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import Header from './Header';
 import './App.css';
 
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <div className='app'>
+      {/* Header */}
+      <Header />
+      {/* Home */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyD3rmBZm8ZRLGyZYIiZnth6mH2JjL43xz0',
+  authDomain: 'challange-2d78b.firebaseapp.com',
+  databaseURL: 'https://challange-2d78b.firebaseio.com',
+  projectId: 'challange-2d78b',
+  storageBucket: 'challange-2d78b.appspot.com',
+  messagingSenderId: '698577204647',
+  appId: '1:698577204647:web:956c3d75fc727ad7b2f282',
+  measurementId: 'G-2EZJP1LC8N',
+};
